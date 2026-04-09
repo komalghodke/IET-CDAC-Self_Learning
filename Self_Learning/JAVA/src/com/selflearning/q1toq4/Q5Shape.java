9c823786a64f706ffb266e219b3c261a23fb023a
komalghodke <komalghodke99@gmail.com>
2026-04-09 19:48:30 +0530

    Added Java Self Assignment Solution

---
+++ Self_Learning/JAVA/src/com/selflearning/q1toq4/Q5Shape.java
@@ -0,0 +1,10 @@
+package com.selflearning.q1toq4;
+
+/*
+Abstraction:
+Abstract class with abstract method draw().
+*/
+public abstract class Q5Shape {
+    public abstract void draw();
+}
+
---
+++ Self_Learning/JAVA/src/com/selflearning/q1toq4/Rectangle.java
@@ -0,0 +1,13 @@
+package com.selflearning.q1toq4;
+
+/*
+Rectangle inherits Shape and overrides draw().
+Demonstrates Inheritance and Polymorphism.
+*/
+public class Rectangle extends Q5Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+
