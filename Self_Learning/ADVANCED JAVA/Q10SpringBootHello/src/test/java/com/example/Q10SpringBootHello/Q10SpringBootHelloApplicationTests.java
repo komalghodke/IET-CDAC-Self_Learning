1af6676c4c6e2a7dd78b53dccdaa44a25fa47a27
komalghodke <komalghodke99@gmail.com>
2026-06-10 15:36:08 +0530

    added minor changes

---
+++ Self_Learning/ADVANCED JAVA/Q10SpringBootHello/src/test/java/com/example/Q10SpringBootHello/Q10SpringBootHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Q10SpringBootHello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q10SpringBootHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
