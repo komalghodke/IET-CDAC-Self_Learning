b3c7df75ffc961882edf13b86dd5b30bc21b0aa4
komalghodke <komalghodke99@gmail.com>
2026-06-10 10:55:48 +0530

    Added Q13 solution

---
+++ Self_Learning/ADVANCED JAVA/Q12CrudThymeleaf/src/main/java/com/demo/Q12CrudThymeleafApplication.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q12CrudThymeleafApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Q12CrudThymeleafApplication.class, args);
+	}
+}
--- Self_Learning/ADVANCED JAVA/Q9LibrarySystemSpringWeb/src/main/java/com/demo/dao/BookDAO.java
+++ Self_Learning/ADVANCED JAVA/Q9LibrarySystemSpringWeb/src/main/java/com/demo/dao/BookDAO.java
@@ -7,29 +7,42 @@ import org.hibernate.cfg.Configuration;
 import java.util.List;
 
 public class BookDAO {
-    private static SessionFactory factory = new Configuration().configure().buildSessionFactory();
+	private static SessionFactory factory;
 
-    public void addBook(Book b) {
-        Session session = factory.openSession();
-        session.beginTransaction();
-        session.persist(b);
-        session.getTransaction().commit();
-        session.close();
-    }
+	// Static initializer to load Hibernate configuration
+	static {
+		try {
+			factory = new Configuration().configure().buildSessionFactory();
+		} catch (Throwable ex) {
+			throw new ExceptionInInitializerError("Failed to create SessionFactory: " + ex);
+		}
+	}
 
-    public List<Book> getAllBooks() {
-        Session session = factory.openSession();
-        List<Book> list = session.createQuery("from Book", Book.class).list();
-        session.close();
-        return list;
-    }
+	// Add a new book
+	public void addBook(Book b) {
+		Session session = factory.openSession();
+		session.beginTransaction();
+		session.persist(b);
+		session.getTransaction().commit();
+		session.close();
+	}
 
-    public void deleteBook(int id) {
-        Session session = factory.openSession();
-        session.beginTransaction();
-        Book b = session.get(Book.class, id);
-        if(b != null) session.remove(b);
-        session.getTransaction().commit();
-        session.close();
-    }
+	// Get all books
+	public List<Book> getAllBooks() {
+		Session session = factory.openSession();
+		List<Book> list = session.createQuery("from Book", Book.class).list();
+		session.close();
+		return list;
+	}
+
+	// Delete a book by ID
+	public void deleteBook(int id) {
+		Session session = factory.openSession();
+		session.beginTransaction();
+		Book b = session.get(Book.class, id);
+		if (b != null)
+			session.remove(b);
+		session.getTransaction().commit();
+		session.close();
+	}
 }
